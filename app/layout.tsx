368e18947cb10a93413661b0f4d73dbb0ebd4b77
Haidar <afiq.haidar12@gmail.com>
2023-07-22 14:29:14 +0700

    fetch listing

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default async function RootLayout({
           <RegisterModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        {children}
+        <div className="pb-28 pt-28">{children}</div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,31 @@
-export default function Home() {
-  return <div>Olla</div>;
+import getCurrentUser from "./actions/getCurrentUser";
+import getListing from "./actions/getListing";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
+
+export default async function Home() {
+  const listings = await getListing();
+  const currentUser = await getCurrentUser();
+  const isEmpty = true;
+
+  if (listings.length == 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 gap-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          {listings.map((listing: any) => {
+            return <ListingCard key={listing.id} data={listing} />;
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
